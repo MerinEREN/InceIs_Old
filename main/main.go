b3700a4e01c4885025ce162d38210f452427e88b
MerinEREN <merineren@gmail.com>
2016-04-27 14:51:07 +0300

    PtoP

--- main/main.go
+++ main/main.go
@@ -1,104 +1,84 @@
 package main
 
 import (
-	"errors"
+	// "errors"
 	"fmt"
+	valid "github.com/asaskevich/govalidator"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
+	// "strings"
+	"time"
 )
 
 const (
-	mongoDBLocal = "mongodb://localhost"
+	// mongoDBLocal = "mongodb:// localhost"
+	mongoDBLocal = "localhost"
 )
 
-// OO DB Structs
-// Accounts collection docs
-type Accounts struct {
-	Name          string `bson: "name"`
-	LastName      string `bson: "lastName"`
-	Type          string `bson: "type"`
-	CurrentStatus string `bson: "currentStatus"`
-	AccountStatus string `bson: "accountStatus"`
-	About         string `bson: "about"`
-	Tags          Tags
-	Ranks         Ranks
-	Card          Card
-	// Users Users
+var (
+	templates = template.Must(template.ParseGlob("../templates/*.gohtml"))
+	index     = renderTemplate("index")
+	logIn     = renderTemplate("logIn")
+	signUp    = renderTemplate("signUp")
+
+	// CHANGE THE REGEXP BELOW !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	validPath = regexp.MustCompile("^/(|logIn|signUp)$")
+)
+
+// OO Language and Page Sturcts
+// Languages colection
+/* type Languages []Language
+
+type Language struct {
+	Id    string `bson:"id"` // EN, TR ...
+	Pages []Page `bson:"pages"`
 }
 
-type Company struct {
-	Name    string `bson: "name"`
-	Address Address
-}
-
-type Address struct {
-	Details     string `bson : "details"`
-	Borough     string `bson : "borough"`
-	City        string `bson : "city"`
-	Country     string `bson : "country"`
-	Postcode    string `bson : "postcode"`
-	Geolocation Geolocation
-}
-
-type Geolocation struct {
-	Lat  string `bson: "lat"`
-	Long string `bson: "Long"`
-}
-
-type Tags []Tag
-
-type Tag struct {
-	Type string `bson: "type"`
-}
-
-type Ranks []Rank
-
-type Rank struct {
-	Type string `bson: "type"`
-}
+type Pages []Page
 
 type Page struct {
-	Title string
-	Body  []byte
+	Title string `bson:"title"`
+	Body  Body `bson:"body"`
+	// Templates maybe
 }
 
-type Card struct {
-	CreditCards CreditCards
-	DebitCards  DebitCards
+type Body struct {
+	Header Header `bson:"header"`
+	// Others ...
+	Footer Footer `bson:"footer"`
 }
 
-type CreditCards []CreditCard
-
-type CreditCard struct {
-	HolderName string `bson: "holderName"`
-	No         string `bson: "no"`
-	ExpMonth   string `bson: "expMonth"`
-	ExpYear    string `bson: "expYear"`
-	CVV        string `bson: "cvv"`
+type Header struct {
+	// Should be created their own types in the future !!!!!!!!!!!!!!!!!!!!
+	SearchPlaceHolder []byte `bson:"searchPlaceHolder"`
+	MenuButtonText []byte `bson:"menuButtonText"`
 }
 
-type DebitCards []DebitCard
+type Footer struct {
+	// Should be created their own types in the future !!!!!!!!!!!!!!!!!!!!
+	SearchPlaceHolder []byte `bson:"searchPlaceHolder"`
+	MenuButtonText []byte `bson:"menuButtonText"`
+} */
 
-type DebitCard struct {
-	HolderName string `bson: "holderName"`
-	No         string `bson: "no"`
-	ExpMonth   string `bson: "expMonth"`
-	ExpYear    string `bson: "expYear"`
-	CVV        string `bson: "cvv"`
-}
-
-func (p *Page) save() error {
+/* func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
+} */
+
+type Page struct {
+	Title string `bson:"title"`
+	Body  []byte `bson:"body"`
 }
 
+// USE THIS TO GET PAGE CONTENTS FOR EVERY LANG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
+	filename := title + ".gohtml"
+	body, err := ioutil.ReadFile("../templates/" + filename)
 	if err != nil {
 		return nil, err
 	}
@@ -106,100 +86,347 @@ func loadPage(title string) (*Page, error) {
 }
 
 func main() {
-	p1 := &Page{Title: "Test Page", Body: []byte("This is a sample Page.")}
-	if p1.save() != nil {
-		fmt.Println("Shit happens!!!!!!")
-	}
-	p2, _ := loadPage("Test Page")
-	fmt.Println(string(p2.Body))
-
 	session, err := mgo.Dial(mongoDBLocal)
 	if err != nil {
 		fmt.Println(session, err)
+		panic(err)
 	}
-
+	defer session.Close()
 	c := session.DB("OO").C("accounts")
+	// m := bson.M{}
 
-	c.Insert(&Accounts{Name: "Merin", LastName: "EREN"})
-	var result interface{}
-	c.Find(Accounts{Name: "Merin", LastName: "EREN"}).One(&result)
-	// acc := c.Find(Accounts{Name: "Merin", LastName: "EREN"})
-	// acc := c.Find(bson.M{"name": "Merin"})
-	fmt.Println(result)
+	// Us this as a http.HandlerFunc
+	signUpTemp(c)
 
-	/*res, err := http.Get("http://www.google.com/robots.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	robots, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s", robots)*/
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", makeHandler(indexHandler))
+	mux.HandleFunc("/logIn", makeHandler(logInHandler))
+	mux.HandleFunc("/signUp", makeHandler(signUpHandler))
+	/* if mux.PostForm("/logIn", data); err != nil {
+		http.Err(w, "Internal server error while login",
+			http.StatusBadRequest)
+	} */
+	log.Printf("About to listen on 10443. " +
+		"Go to https://192.168.1.100:10443/ " +
+		"or https://localhost:10443/")
+	// Redirecting to a port or a domain etc.
+	go http.ListenAndServe(":8080",
+		http.RedirectHandler("https://192.168.1.100:10443", 301))
+	err = http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", mux)
+	// ListenAndServeTLS always returns a non-nil error !!!
+	log.Fatal(err)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		// The http.Redirect function adds an HTTP status code of
-		//http.StatusFound (302) and a Location header to the HTTP
-		// response.
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+// OO DB Structs
+// Accounts collection
+type Accounts []Account
+
+type Account struct {
+	Name          int       `bson:"name,omitempty"`
+	Type          string    `bson:"type,omitempty"`
+	CurrentStatus string    `bson:"current_status,omitempty"`
+	AccountStatus string    `bson:"account_status,omitempty"`
+	About         string    `bson:"about,omitempty"`
+	Tags          Tags      `bson:"tags,omitempty"`
+	Ranks         Ranks     `bson:"ranks,omitempty"`
+	Card          Card      `bson:"card,omitempty" valid:"creditcard"`
+	Users         Users     `bson:"users,omitempty"`
+	Registered    time.Time `bson:"registered,omitempty"`
+	LastModified  time.Time `bson:"last_modified,omitempty"`
+}
+
+type Company struct {
+	Name    string  `bson:"name,omitempty"`
+	Address Address `bson:"address,omitempty"`
+}
+
+type Address struct {
+	Description string      `bson:"description,omitempty"`
+	Borough     string      `bson:"borough,omitempty"`
+	City        string      `bson:"city,omitempty"`
+	Country     string      `bson:"country,omitempty"`
+	Postcode    string      `bson:"postcode,omitempty"`
+	Geolocation Geolocation `bson:"geolocation,omitempty"`
+}
+
+type Geolocation struct {
+	Lat  string `bson:"lat,omitempty"`  // type could be differnt !!!
+	Long string `bson:"Long,omitempty"` // type could be differnt !!!
+}
+
+type Tags []Tag
+
+type Tag struct {
+	Type string `bson:"type,omitempty"`
+}
+
+type Ranks []Rank
+
+type Rank struct {
+	Type string `bson:"type,omitempty"`
+}
+
+type Card struct {
+	Creditcards Creditcards `bson:"creditcards,omitempty"`
+	Debitcards  Debitcards  `bson:"debitcards,omitempty"`
+}
+
+type Creditcards []Creditcard
+
+type Creditcard struct {
+	HolderName string `bson:"holder_name,omitempty"`
+	No         string `bson:"no,omitempty"`
+	ExpMonth   string `bson:"exp_month,omitempty"`
+	ExpYear    string `bson:"exp_year,omitempty"`
+	CVV        string `bson:"cvv,omitempty"`
+}
+
+type Debitcards []Debitcard
+
+type Debitcard struct {
+	HolderName string `bson:"holder_name,omitempty"`
+	No         string `bson:"no,omitempty"`
+	ExpMonth   string `bson:"exp_month,omitempty"`
+	ExpYear    string `bson:"exp_year,omitempty"`
+	CVV        string `bson:"cvv,omitempty"`
+}
+
+type Users []User
+
+type User struct {
+	Email       string `bson:"email,omitempty"`
+	Password    string `bson:"password,omitempty"`
+	PicturePath string `bson:"picture_path,omitempty"`
+	Name        Name   `bson:"name,omitempty"`
+	Phone       string `bson:"phone,omitempty"` // Should be struct in
+	// the future !!!
+	Status       string       `bson:"status,omitempty"`
+	Type         string       `bson:"type,omitempty"`
+	BirthDate    time.Time    `bson:"birth_date,omitempty"`
+	Registered   time.Time    `bson:"registered,omitempty"`
+	LastModified time.Time    `bson:"last_modified,omitempty"`
+	IsActive     bool         `bson:"is_active,omitempty"`
+	ServicePacks ServicePacks `bson:"service_packs",omitempty"`
+	// 	PurchasedServices PurchasedServices `bson:"purchasedServices,
+	// 	omitempty"`
+}
+
+type Name struct {
+	First string `bson:"first,omitempty"`
+	Last  string `bson:"last,omitempty"`
+}
+
+type ServicePacks []ServicePack
+
+type ServicePack struct {
+	Id             string            `bson:"id,omitempty"`
+	Type           string            `bson:"type,omitempty"`
+	Description    string            `bson:"description,omitempty"`
+	Duration       string            `bson:"duration,omitempty"`
+	Price          Price             `bson:"price,omitempty"`
+	Extras         ServicePackExtras `bson:"extras,omitempty"`
+	Photos         Photos            `bson:"photos,omitempty"`
+	Videos         Videos            `bson:"videos,omitempty"`
+	Tags           Tags              `bson:"tags,omitempty"`
+	Created        time.Time         `bson:"created,omitempty"`
+	LastModified   time.Time         `bson:"last_modified,omitempty"`
+	Status         string            `bson:"status,omitempty"`
+	Evaluation     Evaluation        `bson:"evaluation,omitempty"`
+	CustomerReview string            `bson:"customer_review,omitempty"`
+}
+
+type Price struct {
+	Amount   float64 `bson:amount,omitempty"`
+	Currency string  `bson:currency,omitempty"`
+}
+
+type ServicePackExtras []ServicePackOption
+
+type ServicePackOption struct {
+	Id          string `bson:"id,omitempty"`
+	Description string `bson:"description,omitempty"`
+	Duration    string `bson:"duration,omitempty"`
+	Price       Price  `bson:"price,omitempty"`
+	Photos      Photos `bson:"photos,omitempty"`
+	Videos      Videos `bson:"videos,omitempty"`
+}
+
+type Photos []Photo
+
+type Photo struct {
+	Id           string    `bson:"id,omitempty"`
+	Path         string    `bson:"path,omitempty"`
+	Title        string    `bson:"title,omitempty"`
+	Description  string    `bson:"description,omitempty"`
+	Uploaded     time.Time `bson:"uploaded,omitempty"`
+	LastModified time.Time `bson:"last_modified,omitempty"`
+	Status       string    `bson:"status,omitempty"`
+}
+
+type Videos []Video
+
+type Video struct {
+	Id           string    `bson:"id,omitempty"`
+	Path         string    `bson:"path,omitempty"`
+	Title        string    `bson:"title,omitempty"`
+	Description  string    `bson:"description,omitempty"`
+	Uploaded     time.Time `bson:"uploaded,omitempty"`
+	LastModified time.Time `bson:"last_modified,omitempty"`
+	Status       string    `bson:"status,omitempty"`
+}
+
+type Evaluation struct {
+	Technical     byte `bson:"technical,omitempty"`
+	Communication byte `bson:"communication,omitempty"`
+	Time          byte `bson:"time,omitempty"`
+}
+
+type doc interface {
+	// Use this for all structs
+	// Update()
+	// Upsert()
+	// Delete()
+}
+
+// Make a makeAjaxHandler And Wrap http.HandlerFunc like signUp()
+// Sign Up Handler
+func signUpTemp(c *mgo.Collection) {
+	// Get parameters from HTTP Request !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	email := "ivieren@gmail.com"
+	if !valid.IsEmail(email) {
+		// Send response !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		fmt.Println("Invalid email")
 		return
 	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
+	password := "0018@8100"
+	// Cahange this control and allow special characters !!!!!!!!!!!!!!!!!!
+	if !valid.IsAlphanumeric(password) {
+		// Send response !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		fmt.Println("Invalid password")
+		return
 	}
-	renderTemplate(w, "edit", p)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	/*t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)*/
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	var accountCount int
+	accountCount, err := c.Find(bson.M{"users.email": email}).Count()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		// Send response !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		fmt.Printf("Insert account error, because can't verify email. Error: %v\n",
+		err)
+		return
+	}
+	if accountCount != 0 {
+		// Send response !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		fmt.Printf("Email exists !!!, Count: %v\n", accountCount)
+		return
+	}
+	users := Users{
+		User{
+			Email:        email,
+			Password:     password,
+			Status:       "online",
+			Type:         "admin",
+			IsActive:     true,
+			Registered:   time.Now(),
+			LastModified: time.Now(),
+		},
+	}
+	// Get the account count and use as name !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	acc := Account{
+		Name:          1,
+		CurrentStatus: "available",
+		AccountStatus: "online",
+		Users:         users,
+		Registered:    time.Now(),
+		LastModified:  time.Now(),
+	}
+	err = c.Insert(acc)
+	if err != nil {
+		// Send response !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		fmt.Println("Insert account error")
+		return
 	}
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body") // returns string
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+func renderTemplate(title string) func(w http.ResponseWriter, p *Page) {
+	return func(w http.ResponseWriter, p *Page) {
+		err := templates.ExecuteTemplate(w, title+".gohtml", p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			// maybe log.Fatal(err) or http.Redirect(w, r, "/logIn/", 
+			// http.StatusFound)
+		}
+	}
+}
+
+// MAKE "index" AND "logIn" PAGE AS ONE PAGE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+func indexHandler(w http.ResponseWriter, r *http.Request, s string) {
+	// w.Header().Set("Content-Type", "text/plain")
+	// w.Write([]byte("This is Main page " + s + "\n"))
+	fmt.Printf("index s is %s", s)
+	s = "index"
+	p, err := loadPage(s)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+	index(w, p)
 }
 
-var templates = template.Must(template.ParseFiles("view.html", "edit.html"))
+func logInHandler(w http.ResponseWriter, r *http.Request, s string) {
+	// w.Header().Set("Content-Type", "text/plain")
+	// w.Write([]byte("This is Log In page " + s + "\n"))
+	fmt.Printf("logIn s is %s", s)
+	p, err := loadPage(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	logIn(w, p)
+}
 
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+func signUpHandler(w http.ResponseWriter, r *http.Request, s string) {
+	// w.Header().Set("Content-Type", "text/plain")
+	// w.Write([]byte("This is Sign Up page " + s + "\n"))
+	fmt.Printf("signUp s is %s", s)
+	p, err := loadPage(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	signUp(w, p)
+}
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request,
 	string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
-			http.NotFound(w, r) // Writing "404 Not Found" error
-			// to the HTTP connection.
-			fmt.Println(errors.New("Invalid Page Title"))
+			log.Printf("Invalid Path: %s\n", r.URL.Path)
+			// Writing "404 Not Found" error to the HTTP connection
+			http.NotFound(w, r)
 			return
 		}
 		for _, val := range m {
 			fmt.Println(val)
 		}
-		fn(w, r, m[2])
+		switch len(m) {
+		case 2:
+			fn(w, r, m[1])
+		case 3:
+			fn(w, r, m[2])
+		default:
+			fn(w, r, "index")
+		}
 	}
+	/* urlSlice := strings.Split(r.URL.Path, "/")
+	for _, v := range urlSlice {
+		fmt.Println(v)
+	}
+	switch len(urlSlice) {
+	case 2:
+		fn(w, r, urlSlice[1])
+	case 3:
+		fn(w, r, urlSlice[2])
+	default:
+		fn(w, r, "index")
+	} */
 }
